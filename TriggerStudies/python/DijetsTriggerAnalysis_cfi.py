98602e3a7720bc0376cd16efbfa1ea47ac81977a
Diego Matos Figueiredo <dmf@cern.ch>
2013-09-12 22:40:40 +0200

    Git conflict.

--- TriggerStudies/python/DijetsTriggerAnalysis_cfi.py
+++ TriggerStudies/python/DijetsTriggerAnalysis_cfi.py
@@ -1,5 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
+DijetsTriggerAnalysis = cms.PSet(
+    gctDigisTag = cms.InputTag("gctDigis"),
+    # ET sum/tower count per HF ring (HFRingEtSums, HFBitCounts)
+    hfRingEtSumThreshold = cms.uint32(8),
+    hfRingBitCountThreshold = cms.uint32(8),
+    l1TriggerNames = cms.vstring('L1_SingleJet36_FwdVeto','L1_SingleJet36','L1_DoubleJet36_Central','L1_DoubleEG5')
+)
+
 """
 DijetsTriggerAnalysis = cms.PSet(
   #gtDigisTag = cms.InputTag("gtDigis"),
@@ -10,12 +18,6 @@ DijetsTriggerAnalysis = cms.PSet(
   hfRingThreshold = cms.uint32(5),
   l1TriggerNames = cms.vstring('L1_SingleJet36_FwdVeto','L1_SingleJet36','L1_DoubleJet36_Central','L1_SingleEG18','L1_DoubleEG5') 
 )
+
 """
 
-DijetsTriggerAnalysis = cms.PSet(
-    gctDigisTag = cms.InputTag("gctDigis"), 
-    # ET sum/tower count per HF ring (HFRingEtSums, HFBitCounts)
-    hfRingEtSumThreshold = cms.uint32(8),
-    hfRingBitCountThreshold = cms.uint32(8),
-    l1TriggerNames = cms.vstring('L1_SingleJet36_FwdVeto','L1_SingleJet36','L1_DoubleJet36_Central','L1_DoubleEG5') 
-)
