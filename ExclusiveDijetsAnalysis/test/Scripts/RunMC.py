e20c284116ee1ce8301b88a3d79c5b239015e028
Eliza Melo Da Costa <eliza@lxplus440.cern.ch>
2014-02-09 00:03:20 +0100

    update

--- ExclusiveDijetsAnalysis/test/Scripts/RunMC.py
+++ ExclusiveDijetsAnalysis/test/Scripts/RunMC.py
@@ -11,13 +11,13 @@ print ''
 # Pythia PU
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_PYTHIA/LowPU/mc_total/mc_pythia_lowpu_0.root\" \"histo_pythia6pu_pT60_60.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoPythia6.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 55781.0683578044 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/Pythia/total/Pythia6QCD.root\" \"histo_pythia6pu_pT60_60.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoPythia6.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 107914.872597126 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 #
 # Herwig PU
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_HERWIG/mc_total/mc_herwig_lowpu_0.root\" \"histo_herwigpu_pT60_60.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 60060.4549155978 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/Herwig/total/HerwigQCD.root\" \"histo_herwigpu_pT60_60.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 112635.873938081 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 
 #----------------------------------------------------------->>>
@@ -26,13 +26,13 @@ os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_HERWIG/mc_tot
 # Pythia PU0
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_PYTHIA/LowPU/mc_total/mc_pythia_lowpu_0.root\" \"histo_pythia6pu_pT60_60_pu0.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoPythia6.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 365676.168663604 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/Pythia/total/Pythia6QCD.root\" \"histo_pythia6pu_pT60_60_pu0.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoPythia6.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 365676.168663604 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 #
 # Herwig PU0
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_HERWIG/mc_total/mc_herwig_lowpu_0.root\" \"histo_herwigpu_pT60_60_pu0.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 402676.822340036 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/Herwig/total/HerwigQCD.root\" \"histo_herwigpu_pT60_60_pu0.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 402676.822340036 \"mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 
 #----------------------------------------------------------->>>
@@ -41,25 +41,25 @@ os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_HERWIG/mc_tot
 # ExHume 
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_EXHUME/mc_exhume_0.root\" \"histo_exhume.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 0.023802870674982 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/ExHume/ExHume.root\" \"histo_exhume.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 0.021019933333333 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 #
 # Pomwig 
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_POMWIG/mc_pomwig_dpe_0.root\" \"histo_pomwig.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 9.58245938037161 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/Pomwig/PomwigPDE.root\" \"histo_pomwig.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 8.52957858752472 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 #
 # Pompyt Plus 
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_POMPYT/mc_pompyt_plus_0.root\" \"histo_pompytplus.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 109.678367396884 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/PompytPlus/PompytPlus.root\" \"histo_pompytplus.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 97.5062670299728 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 #
 # Pompyt Minus 
 #
 
-os.system("./ExclusiveDijet \"/storage1/eliza/PATTuples_Mar2013/mc_POMPYT/mc_pompyt_minus_0.root\" \"histo_pompytminus.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 104.729539408482 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
+os.system("./ExclusiveDijet \"/storage1/eliza/Samples/ExclusiveDijets/PompytMinus/PompytMinus.root\" \"histo_pompytminus.root\" \"exclusiveDijetsAnalysisTTree\" \"no_trigger\" \"no_multiple_pileup\" \"none\" \"no_pileup_correction\" \"pu_MultiJets_completejson_excldijets_march2013_68mb_bin24.root\" \"puHistoHerwig.root\" \"effMinBias2010RunB_hf7pf_exclusive.root\" \"histo_effTriggerMultijetsRunB_RefDijetAve50U_And30U_castor.root\" \"no_cut_correction\" \"no_trigger_correction\" \"mc_lumi_weight\" 92.7067357512952 \"no_mc_event_weight\" 1 0 60 60 \"no_castor\" \"channelcorrection.root\" 1.0 \"preselection\"")
 
 
 print ''
