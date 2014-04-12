7e0b18efd4655dca1af5c531955e20fb793c42bf
Diego Matos Figueiredo <dmf@cern.ch>
2014-04-12 08:38:07 +0200

    Diffractive W Package

---
+++ DiffractiveWAnalysis/python/DiffractiveWAnalysis_cfi.py
@@ -0,0 +1,36 @@
+import FWCore.ParameterSet.Config as cms
+
+from ForwardAnalysis.DiffractiveWAnalysis.pfThresholds_cfi import pfThresholds
+
+DiffractiveWAnalysis = cms.PSet(
+    hltPaths = cms.vstring(''),
+    TriggerResultsTag = cms.InputTag("TriggerResults::HLT"),
+    electronTag = cms.InputTag("gsfElectrons"),
+    muonTag = cms.InputTag("muons"),
+    metTag = cms.InputTag("pfMet"),
+    patmetTag = cms.InputTag("patpfMet"),
+    genTag = cms.InputTag("genChargedParticles"),
+    pfTag = cms.InputTag("pfCandidateNoiseThresholds"), 
+    PVtxCollectionTag=cms.InputTag('goodOfflinePrimaryVertices'),
+    zdcHitsTag = cms.InputTag("zdcreco"),
+    castorHitsTag = cms.InputTag("castorRecHitCorrector"),
+    RunMC = cms.untracked.bool(False),
+    RunZDC = cms.untracked.bool(False),
+    RunCastor = cms.untracked.bool(True),
+    RunA = cms.untracked.bool(True), # When RunA == RunB == True/False (All Channels). RunA=True and RunB=False (Good Channels Run A)/ RunA=False and RunB=True (Good Channels RunB)
+    RunB = cms.untracked.bool(True),
+    EachTower = cms.untracked.bool(True),
+    fCGeVCastor = cms.double(0.015), # for MC we need use 0.9375 (0.015/0.016)  
+    castorThreshold = cms.double(0.364), # 364 MeV for 2010
+    pTPFThresholdCharged = cms.double(0.1),
+    energyPFThresholdBar = cms.double(1.5),
+    energyPFThresholdEnd = cms.double(3.5),
+    energyPFThresholdHF = cms.double(4.0),
+    energyThresholdHB = cms.double(1.5),
+    energyThresholdHE = cms.double(2.0),
+    energyThresholdHF = cms.double(4.0),
+    energyThresholdEB = cms.double(0.6),
+    energyThresholdEE = cms.double(1.5),
+    CaloTowerTag = cms.InputTag("towerMaker"),
+    TrackTag = cms.InputTag("analysisTracks")
+    )
