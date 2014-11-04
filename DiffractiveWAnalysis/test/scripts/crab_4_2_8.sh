03a4599a54caaf10cc4c3d0da4583d52522a0fd6
dfigueiredo <dmf@cern.ch>
2014-11-04 15:09:33 +0100

    bash script

---
+++ DiffractiveWAnalysis/test/scripts/crab_4_2_8.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=${PYTHONPATH}:/afs/cern.ch/cms/slc5_amd64_gcc434/external/py2-pycurl/7.19.0-cms2/lib/python2.6/site-packages/
+
+export PYTHONPATH=${PYTHONPATH}:/afs/cern.ch/cms/slc5_amd64_gcc434/external/py2-cjson/1.0.5/lib/python2.6/site-packages/
