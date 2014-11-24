1e9e5b86fd6e87a54e687d092e6685b4636b0e87
dfigueiredo <dmf@cern.ch>
2014-11-24 16:40:16 +0100

    update parameters

--- DiffractiveWAnalysis/test/Running/RunAll.py
+++ DiffractiveWAnalysis/test/Running/RunAll.py
@@ -41,37 +41,37 @@ os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_
 # Pomwig Plus WtoENu
 #
 
-os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoE_Plus/pomwigWtoEPlus.root\" \"diffractiveWAnalysisTTree\" \"histo_PompytPlus_electron_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoElectron\" -2.0 1.0 \"channelcorrector.root\"")
+os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoE_Plus/pomwigWtoEPlus.root\" \"diffractiveWAnalysisTTree\" \"histo_PomwigPlus_electron_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoElectron\" -2.0 1.0 \"channelcorrector.root\" \"pf\" \"puMC.root\" \"puData.root\" ")
 
 #
 # Pomwig Minus WtoENu
 #
 
-os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoE_Minus/pomwigWtoEMinus.root\" \"diffractiveWAnalysisTTree\" \"histo_PompytMinus_electron_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoElectron\" -2.0 1.0 \"channelcorrector.root\"")
+os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoE_Minus/pomwigWtoEMinus.root\" \"diffractiveWAnalysisTTree\" \"histo_PomwigMinus_electron_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoElectron\" -2.0 1.0 \"channelcorrector.root\" \"pf\" \"puMC.root\" \"puData.root\" ")
 
 #
 # Pomwig Plus WtoMuNu
 #
 
-os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoMu_Plus/pomwigWtoMuPlus.root\" \"diffractiveWAnalysisTTree\" \"histo_PompytPlus_muon_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoMuon\" -2.0 1.0 \"channelcorrector.root\"")
+os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoMu_Plus/pomwigWtoMuPlus.root\" \"diffractiveWAnalysisTTree\" \"histo_PomwigPlus_muon_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoMuon\" -2.0 1.0 \"channelcorrector.root\" \"pf\" \"puMC.root\" \"puData.root\" ")
 
 #
 # Pomwig Minus WtoMuNu
 #
 
-os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoMu_Minus/pomwigWtoMuMinus.root\" \"diffractiveWAnalysisTTree\" \"histo_PompytMinus_muon_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoMuon\" -2.0 1.0 \"channelcorrector.root\"")
+os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/PomwigWtoMu_Minus/pomwigWtoMuMinus.root\" \"diffractiveWAnalysisTTree\" \"histo_PomwigMinus_muon_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoMuon\" -2.0 1.0 \"channelcorrector.root\" \"pf\" \"puMC.root\" \"puData.root\" ")
 
 #
 # WToENu
 #
 
-os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/WtoENu/pythiaWtoE.root\" \"diffractiveWAnalysisTTree\" \"histo_DyToEE_Reco.root\" \"no_trigger_correction\" 0 25.0 25.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoElectron\" -2.0 1.0 \"channelcorrector.root\"")
+os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/WtoENu/pythiaWtoE.root\" \"diffractiveWAnalysisTTree\" \"histo_WtoENu_Reco.root\" \"no_trigger_correction\" 0 25.0 25.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoElectron\" -2.0 1.0 \"channelcorrector.root\" \"pf\" \"puMC.root\" \"puData.root\" ")
 
 #
 # WToMuNu
 #
 
-os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/WtoMuNu/pythiaWtoMu.root\" \"diffractiveWAnalysisTTree\" \"histo_DyToMuMu_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoMuon\" -2.0 1.0 \"channelcorrector.root\"")
+os.system("./DiffractiveW \"/storage/dmf/uerj-1/Samples/DiffractiveW/4_november_2014/WtoMuNu/pythiaWtoMu.root\" \"diffractiveWAnalysisTTree\" \"histo_WtoMuNu_Reco.root\" \"no_trigger_correction\" 0 20.0 20.0 1 \"no_multiple_pileup\" \"mc_lumi_weight\" 1.0 \"RecoMuon\" -2.0 1.0 \"channelcorrector.root\" \"pf\" \"puMC.root\" \"puData.root\" ")
 
 print ''
 print '@@@@@@@@@@@@@@@@@@'
