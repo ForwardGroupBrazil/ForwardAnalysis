95c4ef9283d7622013c00cf931c3f36a9f20fac1
dfigueiredo <dmf@cern.ch>
2014-11-24 11:34:53 +0100

    Lumicalc script

---
+++ DiffractiveZAnalysis/test/scripts/LumiCalcAll.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010A_P1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Photon10_L1R" > EleA_HLT_Photon10_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010A_P1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Photon15_Cleaned_L1R" > EleA_HLT_Photon15_Cleaned_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010A_P1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele15_SW_CaloEleId_L1R" > EleA_HLT_Ele15_SW_CaloEleId_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010A_P1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele17_SW_CaloEleId_L1R" > EleA_HLT_Ele17_SW_CaloEleId_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010A_P1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele17_SW_TightEleId_L1R" > EleA_HLT_Ele17_SW_TightEleId_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010A_P1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele22_SW_TighterCaloIdIsol_L1R_v1" > EleA_HLT_Ele22_SW_TighterCaloIdIsol_L1R_v1.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010A_P1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele22_SW_TighterCaloIdIsol_L1R_v2" > EleA_HLT_Ele22_SW_TighterCaloIdIsol_L1R_v2.txt
+
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010B_P2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Photon10_L1R" > EleB_HLT_Photon10_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010B_P2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Photon15_Cleaned_L1R" > EleB_HLT_Photon15_Cleaned_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010B_P2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele15_SW_CaloEleId_L1R" > EleB_HLT_Ele15_SW_CaloEleId_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010B_P2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele17_SW_CaloEleId_L1R" > EleB_HLT_Ele17_SW_CaloEleId_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010B_P2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele17_SW_TightEleId_L1R" > EleB_HLT_Ele17_SW_TightEleId_L1R.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010B_P2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele22_SW_TighterCaloIdIsol_L1R_v1" > EleB_HLT_Ele22_SW_TighterCaloIdIsol_L1R_v1.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZEE/Electron2010B_P2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Ele22_SW_TighterCaloIdIsol_L1R_v2" > EleB_HLT_Ele22_SW_TighterCaloIdIsol_L1R_v2.txt
+
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Mu9" > MuonP1HLT_Mu9.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Mu9_v*" > MuonP1HLT_Mu9_v.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p1_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_DoubleMu3" > MuonP1HLT_DoubleMu3.txt
+
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Mu9" > MuonP2HLT_Mu9.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Mu9_v*" > MuonP2HLT_Mu9_v.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p2_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_DoubleMu3" > MuonP2HLT_DoubleMu3.txt
+
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p3_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Mu15" > MuonP3HLT_Mu15.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p3_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_Mu15_v*" > MuonP3HLT_Mu15_v.txt
+lumiCalc2.py -i /storage/dmf/uerj-1/CrabSubmit/DiffractiveZMuMu/Muon2010_Castor_p3_GoodCastor_4_november_2014/res/lumiSummary.json recorded --hltpath="HLT_DoubleMu5_v*" > MuonP3HLT_DoubleMu5_v.txt
